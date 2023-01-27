f74837a36e8fc5394ad5d5b9bd8c6aab61d80c35
devopstrainer0 <122166097+devopstrainer0@users.noreply.github.com>
2023-01-27 15:38:17 +0530

    Create jupyter_notebook_config.py

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,15 @@
+# The IP address on which to listen
+c.NotebookApp.ip = '0.0.0.0'
+
+# The port on which to run the web server
+c.NotebookApp.port = 8888
+
+# Whether to open in a browser after starting
+c.NotebookApp.open_browser = False
+
+# Whether to allow root to run the notebook
+c.NotebookApp.allow_root = True
+
+# Whether to password protect the notebook
+c.NotebookApp.password = 'sha1:<password_hash>'
+
